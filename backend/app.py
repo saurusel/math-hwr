12a6e1ef3db3ffa85bd4f56e8c9290f2dd613d2a
Matvey <oleninmat@yandex.ru>
2025-10-20 16:33:07 +0500

    feat(api): predict2 options + chore: add Windows smoke (UTF-8) + minor app tweak

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from .api.ws_stream import router as ws_router
 from .api.routes_eval import router as eval_router
 from .api.routes_models import router as models_router
 from .api.routes_leaderboard import router as leaderboard_router
+from .api.routes_predict_ckpt import router as predict2_router
 
 app = FastAPI(title="Math-HWR Backend", version="0.1.0")
 
@@ -25,6 +26,7 @@ app.include_router(ws_router, prefix="/api", tags=["ws"])
 app.include_router(eval_router)
 app.include_router(models_router)
 app.include_router(leaderboard_router)
+app.include_router(predict2_router)
 
 @app.get("/")
 def root():
